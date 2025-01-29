74c5e5706941124862ff6ee5a8bf7e7b01cf06ba
Arjanit Kotorri <kotorriarjanit@gmail.com>
2025-01-29 04:45:23 +0100

    removed the path url strategy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3673733352"
+    serviceWorkerVersion: "2936545840"
   }
 });
